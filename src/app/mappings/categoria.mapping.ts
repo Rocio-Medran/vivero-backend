0ca4bcc8fa04cca12b040643256f74b8acd4125d
Rocio <rociomedran9@gmail.com>
2025-09-12 22:52:41 -0300

    metodo categoria con productos

--- src/app/mappings/categoria.mapping.ts
+++ src/app/mappings/categoria.mapping.ts
@@ -1,9 +1,22 @@
 import { plainToInstance } from "class-transformer";
 import { Categoria } from "../../domain/entities/Categoria";
-import { CategoriaDTO } from "../dtos/categoria.dto";
+import { CategoriaConProductosDTO, CategoriaDTO } from "../dtos/categoria.dto";
+import { toProductoConDetallesDTO } from "./producto.mapping";
 
-export const toCategoriaDTO = (entity: Categoria): CategoriaDTO => 
+export const toCategoriaDTO = (entity: Categoria): CategoriaDTO =>
     plainToInstance(CategoriaDTO, entity, { excludeExtraneousValues: true });
 
-export const toCategoriaDTOs = (entities: Categoria[]): CategoriaDTO[] => 
+export const toCategoriaDTOs = (entities: Categoria[]): CategoriaDTO[] =>
     entities.map(e => toCategoriaDTO(e));
+
+export const toCategoriaConProductosDTO = (entity: Categoria): CategoriaConProductosDTO =>
+    plainToInstance(CategoriaConProductosDTO,
+        {
+            id: entity.id,
+            nombre: entity.nombre,
+            productos: entity.productos ? entity.productos.map(toProductoConDetallesDTO) : []
+        },
+        { excludeExtraneousValues: true });
+
+export const toCategoriaConProductosDTOs = (entities: Categoria[]): CategoriaConProductosDTO[] =>
+    entities.map(e => toCategoriaConProductosDTO(e));
---
+++ src/domain/repositories/interfaces/ICategoriaRepository.ts
@@ -0,0 +1,7 @@
+import { Categoria } from "../../entities/Categoria";
+import { IRepository } from "./IRepository";
+
+export interface ICategoriaRepository extends IRepository<Categoria> {
+    getCategoriaConProductosById(id: number): Promise<Categoria | null>;
+    getCategoriasConProductos(): Promise<Categoria[]>;
+}
