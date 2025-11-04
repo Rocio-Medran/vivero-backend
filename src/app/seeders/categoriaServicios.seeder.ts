cc75fb4d44cbecbdec3563d26a88d6634531becb
Rocio <rociomedran9@gmail.com>
2025-11-04 20:11:14 -0300

    feat(servicios): agrega seeders de servicios con sus imágenes asociadas

--- src/app/seeders/categoriaServicios.seeder.ts
+++ src/app/seeders/categoriaServicios.seeder.ts
@@ -5,15 +5,15 @@ import { CategoriaServicio } from "../../domain/entities/CategoriaServicio";
 export async function seedCategoriaServicios(dataSource: DataSource) {
     const repo = dataSource.getRepository(CategoriaServicio);
     const categoriasServicios = [
-        { nombre: "Maquinarias",
+        { nombre: "Alquiler de Maquinarias",
             imagen_url: "https://res.cloudinary.com/djssc9idq/image/upload/v1760057846/maquinaria_ugpcbp.png",
             imagen2_url: "https://res.cloudinary.com/djssc9idq/image/upload/v1760057847/maquinaria2_da0jf0.png" 
         },
-        { nombre: "Enraizamiento",
+        { nombre: "Cámara de Enraizamiento",
             imagen_url: "https://res.cloudinary.com/djssc9idq/image/upload/v1760057848/camara_u9uwpw.png",
             imagen2_url: "https://res.cloudinary.com/djssc9idq/image/upload/v1760057849/camara2_q1szcd.png"
         },
-        { nombre: "Asesoramiento",
+        { nombre: "Asesoramiento Personalizado",
             imagen_url: "https://res.cloudinary.com/djssc9idq/image/upload/v1760057845/asesoramiento_u8ryuj.png",
             imagen2_url: "https://res.cloudinary.com/djssc9idq/image/upload/v1760057844/asesoramiento_2_ortneq.png"
         }
